08517fec87585229c0407b4db55dbe22098540ca
unknown <Student@305-9.spobip-bel.ru>
2024-11-12 13:13:15 +0300

    message

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.sql.Struct;
+
+public class Main {
+    public static void main(String[] args) {
+
+class Person {
+    String Identification_number;
+    String surname;
+    String name;
+    String Middle_name;
+    String address;
+    String Total_talk_time;
+    String Credit_card_number;
+    String Debt;
+}
+
+
+
+
+
+
+    }
+}
