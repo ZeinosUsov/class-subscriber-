08005f3ce33d670ad3ee40a172d15d6f1f4e9d83
Dima <usovnakarantine@gmail.com>
2024-11-12 13:45:19 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,27 @@ Class abonent{
     private String name;
     private String patronym;
     private String adress;
-    private double
+    private double TotalCallTime;//время звонка
+private String creditCardNumber;//карта
+private double debt;//задолжность
+
+public abonent() String id, String surname, String name, String patronym, String adress,
+    double TotalCallTime, String creditCardNumber, double debt){
+    this.id = id;
+    this.surname = surname;
+    this.name = name;
+    this.patronym = patronym;
+    this.adress = adress;
+    this.TotalCallTime = TotallCalltime;
+    this.creditCardNumber = creditCardNumber;
+    this.debt = debt;
+}
+public abonent(String id, String surname, String name, String patronym, String adress,
+               double TotalCallTime, String creditCardNumber, double debt){
+    this(surname, name, patronym,adress, 0, 0)
+}
+public void setSurname(String surname){
+}
 
 
         }
