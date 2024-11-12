769244616b5d6db57055b8c44e4c8dd69bb8e95b
unknown <Student@305-9.spobip-bel.ru>
2024-11-12 13:24:11 +0300

    abonent

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,13 @@
-import java.sql.Struct;
+import java.util.ArrayList;
+import  java.util.List;
 
-public class Main {
-    public static void main(String[] args) {
-
-class Person {
-    String Identification_number;
-    String surname;
-    String name;
-    String Middle_name;
-    String address;
-    String Total_talk_time;
-    String Credit_card_number;
-    String Debt;
-}
+Class abonent{
+    private String id;
+    private String surname;
+    private String name;
+    private String patronym;
+    private String adress;
+    private double
 
 
-
-
-
-
-    }
-}
+        }
